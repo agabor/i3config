e8ce5f8aa1b889cf9b50555743efe57cfe4ab1fc
Gabor Angyal <angyalgabor@outlook.com>
2018-10-12 09:13:04 +0200

    install mc

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt install fonts-font-awesome i3-wm suckless-tools alsamixergui htop rxvt-unicode fonts-inconsolata
+sudo apt install fonts-font-awesome i3-wm suckless-tools alsamixergui htop rxvt-unicode fonts-inconsolata mc
